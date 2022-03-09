0c1a11caea6d528bda5af5b4842697582f783564
Qi Luan <16888917+qluan1@users.noreply.github.com>
2022-03-08 20:57:40 -0800

    added UI for Rock Paper Scissors Game

--- pages/rockpaperscissors.js
+++ pages/rockpaperscissors.js
@@ -1,62 +1,136 @@
 const ROCKPAPERSCISSORS = ["rock", "paper", "scissors"];
+const player = document.querySelector("#rps");
+const buttonBox = document.querySelector("#button_box");
+const buttonRock = document.querySelector("#rock");
+const buttonPaper = document.querySelector("#paper");
+const buttonScissors = document.querySelector("#scissors");
+const buttonReset = document.querySelector("#rps_resetButton");
+const computerDisplay = document.querySelector("#computer_display");
+const playerDisplay = document.querySelector("#player_display");
+const resultMessage = document.querySelector("#rps_message");
+const gameScores = document.querySelector("#rps_scores");
+const playerDisplayBox = document.querySelector("#player_display_box");
+const computerDisplayBox = document.querySelector("#computer_display_box");
+
+let playerScore = 0;
+let computerScore = 0;
+
+function resetGame(){
+    if (!player.contains(buttonBox)) {
+        player.insertBefore(buttonBox, resultMessage);
+    }
+    playerScore = 0;
+    computerScore = 0;
+    displayChoices(3, 3);
+    displayRoundMessage('');
+    displayScores();
+    playerDisplayBox.style.backgroundColor = "transparent";
+    computerDisplayBox.style.backgroundColor = "transparent";
+}
 
 function computerPlay (){
-    return Math.floor(3 * Math.random());    
+    return Math.floor(3 * Math.random());
 }
 
-function playRound (p1, p2) {
-    // the value of s1 and s2 must be integers from 0, 1, and 2
-    let win;
-    let res;
-    if (p1 - p2 == 1 || p1 - p2 == -2) {
-        res = `You Win! ${ROCKPAPERSCISSORS[p1]} beats ${ROCKPAPERSCISSORS[p2]}`;
-        win = 1;
-    } else if ( p1 == p2) {
-        res = "Draw!";
-        win = 0;
-    } else {
-        res = `You Lose! ${ROCKPAPERSCISSORS[p2]} beats ${ROCKPAPERSCISSORS[p1]}`;
-        win = -1;
-    }
-    return win, res;
-}
-
-function parseSelection (s) {
-    let playerSelection = s.toLowerCase();
-    if (playerSelection === "rock") {
+function playerPlay (s) {
+    if (s === "rock") {
         return 0;
-    } else if (playerSelection === "paper") {
+    } else if (s === "paper") {
         return 1;
-    } else if (playerSelection === "scissors") {
+    } else if (s === "scissors") {
         return 2;
     } else {
         return -1;
     }
 }
 
-function game() {
-    let win, winlog, playerSelection;
-    let score = 0;
-    for (let i = 0; i < 5; i++) {
-        while ( true ){
-            playerSelection = parseSelection(prompt("Please enter your selection from rock, paper, and scissors", "rock"));
-            if (playerSelection != -1) {
-                break;
-            }          
-        }
-        win, winlog = playRound(playerSelection, computerPlay());
-        score += win;
-        console.log(winlog);
+function displayChoices(p1, p2) {
+    switch (p1) {
+        case 0:
+            playerDisplay.textContent = "ROCK";
+            break;
+        case 1:
+            playerDisplay.textContent = "PAPER";
+            break;
+        case 2:
+            playerDisplay.textContent = "SCISSORS";
+            break;
+        default:
+            playerDisplay.textContent = "?";
     }
-    if (score > 0) {
-        alert("You won!");
-    } else if (score == 0) {
-        alert("Draw!");
-    } else {
-        alert("You lost!");
+
+    switch (p2) {
+        case 0:
+            computerDisplay.textContent = "ROCK";
+            break;
+        case 1:
+            computerDisplay.textContent = "PAPER";
+            break;
+        case 2:
+            computerDisplay.textContent = "SCISSORS";
+            break;
+        default:
+            computerDisplay.textContent = "?";       
     }
-    return;
 }
 
-game();
+function displayRoundMessage(m) {
+    resultMessage.textContent = m;
+}
+
+function displayScores(){
+    gameScores.textContent = `You ${playerScore} : ${computerScore} Computer`;
+}
+
+function checkGameState(){
+    if (playerScore == 5) {
+        displayRoundMessage("You Won!");
+        player.removeChild(buttonBox);
+    } else if (computerScore == 5){
+        displayRoundMessage("Computer Won!");
+        player.removeChild(buttonBox);
+    }
+}
+
+function playRound (p1, p2) {
+    // the value of s1 and s2 must be integers from 0, 1, and 2
+    let roundMessage;
+    displayChoices(p1, p2);
+    if (p1 - p2 == 1 || p1 - p2 == -2) {
+        roundMessage = `You Win! ${ROCKPAPERSCISSORS[p1]} beats ${ROCKPAPERSCISSORS[p2]}`;
+        playerScore += 1;
+        playerDisplayBox.style.backgroundColor = "rgb(102, 255, 51)";
+        computerDisplayBox.style.backgroundColor = "rgb(255, 51, 0)";
+    } else if ( p1 == p2) {
+        roundMessage = "Draw!";
+        playerDisplayBox.style.backgroundColor = "rgb(0, 153, 255)";
+        computerDisplayBox.style.backgroundColor = "rgb(0, 153, 255)";
+    } else {
+        roundMessage = `You Lose! ${ROCKPAPERSCISSORS[p2]} beats ${ROCKPAPERSCISSORS[p1]}`;
+        computerScore += 1;
+        playerDisplayBox.style.backgroundColor = "rgb(255, 51, 0)";
+        computerDisplayBox.style.backgroundColor = "rgb(102, 255, 51)";
+    }
+    displayRoundMessage(roundMessage);
+    displayScores();
+    checkGameState();
+}
+
+
+buttonRock.addEventListener('click', () => playRound(playerPlay('rock'), computerPlay()));
+buttonPaper.addEventListener('click', () => playRound(playerPlay('paper'), computerPlay()));
+buttonScissors.addEventListener('click', () => playRound(playerPlay('scissors'), computerPlay()));
+buttonReset.addEventListener('click', () => resetGame());
+
+
+
+
+
+
+
+
+
+
+
+
 
